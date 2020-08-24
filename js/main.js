0ffd30ce01323dc28af6c9d645fb62dc0562d5ee
Andrew Nicholls <andrewnicholls@Andrews-MacBook-Pro.local>
2020-08-24 00:00:33 -0500

    adding search text to page and tryng to add gif count to page

--- js/main.js
+++ js/main.js
@@ -2,20 +2,23 @@
 $(document).ready(() => {
    $('#submit').click(()=>{
       $("#try").empty();
+      $("#gifinput").val($("#search").val());
        let userInput = $("#search").val()
     //    alert(userInput)
        $.ajax({
-       url: `http://api.giphy.com/v1/gifs/search?api_key=ztdTe0th1ULT4ef7vpNO0bTdJ0NUN6Gp&q=${userInput}`
+       url: `http://api.giphy.com/v1/gifs/search?api_key=ztdTe0th1ULT4ef7vpNO0bTdJ0NUN6Gp&q=${userInput}&limit=10`
  })
  .done((res)=>{
      let giphy = res.data;
      $.each (giphy, function (i,e) {
          let gifs =e.images.original.url;
          console.log("index", i, e.images.original.url)
-       $("#try").append(`<li class="flex-item"><img src = "${gifs}" alt="giphy" /></li>`) 
-         // $(body).append(`<div><img src = "${gifs}" /></div>`)
+       $("#try").append(`<img src = "${gifs}" alt="giphy" />`) 
      });
+   //   alert($("#try").children('img').length)
+     var count = ($("#try").children('img').length)
+     $("#gifcount").val($('count').val());
+     alert (count)
     })
  })
 })
-// preview_webp
